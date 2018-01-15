159d6130e4de01df47d7ab96789b1a7aa7a4df79
Elias Panula <elias.panula@gmail.com>
2018-01-15 04:38:52 +0200

    set webpack env variable

--- src/config.js
+++ src/config.js
@@ -1,11 +1,6 @@
-let port = process.env.PORT
-if(!process.env.PORT) 
-    port = 3000
-let url = process.env.API_URI
-
-console.log(process.env.API_URI)
+let url = env.API_URI
+console.log(env.API_URI)
 console.log(url)
-
 module.exports = {
     url
 }
--- webpack.config.js
+++ webpack.config.js
@@ -92,7 +92,8 @@ if (process.env.NODE_ENV === 'production') {
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: '"production"'
+        NODE_ENV: '"production"',
+        API_URI: JSON.stringify(process.env.API_URI)
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
