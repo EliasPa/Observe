9009a6762c94ec138f760ddc9d77fb9492b7b350
Elias Panula <elias.panula@gmail.com>
2018-01-15 04:21:30 +0200

    testing url

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 let port = process.env.PORT
 if(!process.env.PORT) 
     port = 3000
-let url = process.env.API_URI || ('http://localhost:'+ port +'/')
+let url = process.env.API_URI
 /*if(process.env.NODE_ENV === 'development'){
     url = 'https://observation-app.herokuapp.com/'
 }*/
-console.log(url)
+console.log(process.env.API_URI)
 module.exports = {
     url
 }
